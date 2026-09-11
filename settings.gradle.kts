0a96cdbfcf4664c19b73913b08d4587e6f5d84be
kennethIve <kennethIve@users.noreply.github.com>
2026-09-11 14:55:56 +0000

    settings.gradle.kts: single plugins block (foojay 1.0 + platform settings).

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-rootProject.name = "forge-bpmn"
-
 pluginManagement {
     repositories {
         mavenCentral()
@@ -9,4 +7,7 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.jetbrains.intellij.platform.settings") version "2.18.1"
 }
+
+rootProject.name = "forge-bpmn"
